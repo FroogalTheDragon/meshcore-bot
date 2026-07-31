18616afc61621ffc2f904ccd15568ca64b103855
FroogalTheDragon <codecannamw@gmail.com>
2026-07-30 20:48:16 -0600

    Added tests for hamhelper command.

---
+++ tests/commands/test_hamhelper_command.py
@@ -0,0 +1,19 @@
+""" Tests for modules.commands.hamhelper_command.py """
+
+class TestHamhelper:
+    def test_get_question_pool(self, command_mock_bot):
+        pass
+    
+    def test_generate_question(self, command_mock_bot):
+        pass
+    
+    def test_record_result(self, command_mock_bot):
+        pass
+    
+    def test_get_leaderboard_data(self, command_mock_bot):
+        pass
+    
+    def test_get_figure_url(self, command_mock_bot):
+        pass
+    
+    
